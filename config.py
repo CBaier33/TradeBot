706d0a5ab5b50dcfa374121d3b32110fe59dd6f8
Christopher Baier <baier.tech@outlook.com>
2023-09-05 13:54:37 -0400

    config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-api_key = 'PKOK81QGPPHPOUS8FNFP'
-api_secret = 't1wQoMGlLOb4YsecfkBBLXG2YVOXE2vl9BAfg3Pz'
-base_url = 'https://paper-api.alpaca.markets'
+api_key = 'ENTER API KEY'
+api_secret = 'ENTER API SECRET'
+base_url = 'ENTER URL' 
